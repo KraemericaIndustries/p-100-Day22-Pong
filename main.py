2cbef116adb8abf5fb131ee0222a63dd4b678ddb
KraemericaIndustries <bob.kraemer.git@gmail.com>
2024-09-12 18:11:04 -0400

    Initial commit.  Paddles responding.  Ball moving.  Initial tests successful

---
+++ main.py
@@ -0,0 +1,30 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
+screen = Screen()
+screen.setup(width = 800, height = 600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle(350)
+l_paddle = Paddle(-350)
+ball = Ball()
+
+screen.listen()
+screen.onkey(key = "Up", fun=r_paddle.go_up)
+screen.onkey(key = "Down", fun=r_paddle.go_down)
+screen.onkey(key = "w", fun=l_paddle.go_up)
+screen.onkey(key = "s", fun=l_paddle.go_down)
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+
+screen.exitonclick()
