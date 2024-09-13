5ddb4625b354dc6fe2a5252041379f75fb680513
KraemericaIndustries <bob.kraemer.git@gmail.com>
2024-09-13 13:30:54 -0400

    ADD FEATURE:  Ball speed now increases each time it hits a paddle.  Initial tests successful

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width = 800, height = 600)
@@ -12,17 +13,18 @@ screen.tracer(0)
 r_paddle = Paddle(350)
 l_paddle = Paddle(-350)
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(key = "Up", fun=r_paddle.go_up)
-screen.onkey(key = "Down", fun=r_paddle.go_down)
-screen.onkey(key = "w", fun=l_paddle.go_up)
-screen.onkey(key = "s", fun=l_paddle.go_down)
+screen.onkeypress(key = "Up", fun=r_paddle.go_up)
+screen.onkeypress(key = "Down", fun=r_paddle.go_down)
+screen.onkeypress(key = "w", fun=l_paddle.go_up)
+screen.onkeypress(key = "s", fun=l_paddle.go_down)
 
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -31,16 +33,18 @@ while game_is_on:
         # Ball needs to bounce
         ball.bounce_y()
 
-    # Detect collision with right paddle
+    # Detect collision with a paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
     # Detect when right paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     # Detect when right paddle misses
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
